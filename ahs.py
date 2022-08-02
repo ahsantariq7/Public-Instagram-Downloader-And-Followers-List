1b43fbbf3d58e3b804823ae60ef90fedf685b75e
Engr.Ahsan Tariq <109543910+ahsantariq7@users.noreply.github.com>
2022-08-01 23:43:05 -0700

    Update ahs.py

--- ahs.py
+++ ahs.py
@@ -3,8 +3,8 @@ from instaloader import Profile,Post
 
 instance=instaloader.Instaloader()
 
-usersearch=input("Enter the username of the account you want to search :")
-instance.login(user="",passwd="")
+usersearch=input("Enter the username of the account you want to search :")#enter username of id you want to download and 
+instance.login(user="",passwd="")  #write username and password there of Your id
 profile = instaloader.Profile.from_username(instance.context, usersearch)
 
 # Print list of followees
